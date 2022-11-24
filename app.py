48de95de0edf7bdafd6fa6b3dcd5096a3aa609a5
awoisoak <awoisoak@gmail.com>
2022-11-24 10:43:57 +0900

    Add Prometheus metrics

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import os, platform, socket, modules.repo as repo, modules.utils as utils
 from tokenize import Name
+from flask_prometheus_metrics import register_metrics
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+
 
 from flask import Flask, render_template, send_from_directory
 
@@ -23,6 +28,21 @@ def get_gallery():
             node = platform.node()
             )
 
+
+############## Metrics ##############
+# Provide app's version and deploy environment/config name to set a gauge metric
+register_metrics(app, app_version="v0.0.0", app_config="staging")
+
+def create_dispatcher() -> DispatcherMiddleware:
+    # App factory for dispatcher middleware managing multiple WSGI apps
+    return DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+######################################
+
+
 if __name__ == "__main__":
-    # Run Flask Application
-    app.run(host="0.0.0.0", port=9000)
+    run_simple(
+        "localhost",
+        9000,
+        create_dispatcher()
+    )
+
